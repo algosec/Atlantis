a0c7aa321c74e368c0a87d0ad6a11676ac799097
Yael Magrafta <83502821+YaelMagrafta@users.noreply.github.com>
2023-05-04 17:00:59 +0300

    Update globalConfig_Template.ts

--- resources/templates/globalConfig_Template.ts
+++ resources/templates/globalConfig_Template.ts
@@ -5,40 +5,47 @@ export const globalConfig = {
     },
     emailDetails: {
         domain: "<organization email such as @gmail. example: @algosec>",
-        sendMessageUrl: "<url address to send emails from ??? >"
+        sendMessageUrl: "< use  Microsoft's Power Automate to generate this url. more info under Atlantis/docs/email and teams integrations.md >"
     },
-    jiraUrl: "<organizations jira full url>",
-    basicApplicationUrl: " <Atlantis host url adress> ",
+    jiraUrl: "<organizations jira's full url>",
+    basicApplicationUrl: " < Youre wished Atlantis host url address> ",
     JWTKeys: {
         privateKey: '<JWT private key> ',
         publicKey: '<JWT public key> '
+		// generate your keys.
+		// You can generate jwt keys using: 
+		// openssl genrsa -out jwt.key 2048
+		// openssl rsa -in jwt.key -outform PEM -pubout -out jwt.crt
     },
     JWTAllureToken: {
         nonProduction : "<token with authenticated user details>",
         production : "<token with authenticated user details>"
+		// generate tokens (using your private and public key) with the following information:
+		// header: 
+		//		{
+		//			"alg": "RS256",
+		//		   	"typ": "JWT"
+		//		}
+		// payload:
+		// 		{
+		//			"userId": "< a uuid of a valid user. Generate this id and use it later for your first user in the db >",
+		//			"tenantId": "< your tenant id from above >",
+		//			"roles": [
+		//			"Admin"
+		//			],
+		//			"iat": 1636551082,
+		//			"iss": "ms-auth",
+		//			"sub": "<any uuid> "
+		//		} 
     },
     teams : {
-        postMessageURL : "<url with post action to team>",
-        postReplayURL : "<currently not in use, can be null>",
+        postMessageURL : "< use  Microsoft's Power Automate to generate this url. more info under Atlantis/docs/email and teams integrations.md >",
+        postReplayURL : "<currently not in use, should be null>",
         secrets : {
             "<version1>" : "<secret>",
             "<version2>" : "<secret>"
       }
     },
-    // a list of buckets, will appear as components
-    buckets: [
-        {
-            title: "<first bucket title>",
-            prefix: ["<first bucket prefix>"],
-            suffix: []
-        },
-        {
-            title: "<second bucket title>",
-            prefix: ["<second bucket prefix>"],
-            suffix: []
-        }
-
-    ],
     localLogin: {
         allowInProduction: true
         }
